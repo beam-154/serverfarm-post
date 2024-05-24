fb0800fc4b853dd93c8e4b2c19a03c3e98d1904d
Caleb <caleb.zack154@gmail.com>
2024-05-24 15:41:07 -0700

    add auth util

---
+++ auth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+const secretKey = 'secret_key_for_server_farm_app';
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) {
+    return res.sendStatus(401);
+  }
+  jwt.verify(token, secretKey, (err, user) => {
+    if (err) {
+      return res.sendStatus(403);
+    }
+    req.user = user;
+    next();
+  });
+};
+
+module.exports = { authenticateToken };
