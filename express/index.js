965e11212f6cbe57819a2c783755679400540730
Caleb <caleb.zack154@gmail.com>
2024-05-26 12:33:24 -0700

    add cors option to express server

--- express/index.js
+++ express/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,6 +12,7 @@ const postsRouter = require('./routes/posts');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
